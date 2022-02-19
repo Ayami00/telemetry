b089ec1da474b3eed7eaed2303b6e64f316e9f2e
Ayami00 <I79606606960@yandex.ru>
2022-02-19 11:36:14 +0300

    para

---
+++ Zadanie_1/Урок_ч1.py
@@ -0,0 +1,10 @@
+Stroka = 'Вы кто такие?'
+
+print(Stroka[::-1])
+
+print(Stroka.index('и'))
+print(Stroka.lower())
+print(Stroka.upper())
+print(len(Stroka))
+print(Stroka.find('кто'))
+print(Stroka.count('к'))
---
+++ Zadanie_1/Урок_ч2.py
@@ -0,0 +1,43 @@
+####0voc = {1 : 'value', 'prineter': 3, 'price': 499}
+#voc2 = {5: 100, 'grippa': 12}
+
+#voc['xerox'] = 55
+#voc['prineter'] = 200
+
+#voc.update(voc2)
+
+#print(voc)
+#print(voc.get('prineter'))
+
+##1massive = [1, 2, 3, 4, 9, 11, 45]
+
+#set1 = {1, 2, 3, 4, 5, 6, 7, 8}
+#set2 = set(massive)
+
+#set3 = set1 - set2
+
+#set1.add('set2')
+
+#print(set1)
+
+###2massive = [1, 2, 3, 4, 5, 'today', False]
+#massive2 = [1, 2, 3, 4]
+#massive3 = massive2
+#massive2.append(50)
+
+#massive3.append(100)
+
+#print(massive2[::-1])
+#print(massive3)
+#print(len(massive))
+
+#massive.append(7)
+
+#print(len(massive))
+#print(massive)
+#massive[2] = 5
+
+#print(massive)
+
+#massive.insert(2, 10)
+#print(massive)
