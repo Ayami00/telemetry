59ae74387e76d280b3afc1b3ee093fc2fdd2abed
Ayami00 <I79606606960@yandex.ru>
2022-02-26 13:26:26 +0300

    33

--- Zadanie________3/6.py
+++ Zadanie________3/6.py
@@ -1,3 +1,18 @@
 #Реализовать функцию int_func(), принимающую слово из маленьких
 #латинских букв и возвращающую его же, но с прописной первой буквой.
 #Например, print(int_func(‘text’)) -> Text.
+#--------------------------------------------
+def fun(a):
+    return a.title()
+print(fun("AAA sSS mmM"))
+#---------------------------------------
+def fun1(a):
+    x = a.split(' ')
+    k = []
+    for i in x:
+        el = str(i)
+        x = el[:1].upper()
+        z = x + el[1:]
+        k.append(z)
+    return k
+print(fun1("sss mmmm aaa"))
