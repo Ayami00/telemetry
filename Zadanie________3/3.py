8777638cc8f07c0850f63e4041866df248e8191f
Ayami00 <I79606606960@yandex.ru>
2022-02-26 12:41:53 +0300

    3

--- Zadanie________3/3.py
+++ Zadanie________3/3.py
@@ -1,2 +1,14 @@
 #Реализовать функцию my_func(), которая принимает три позиционных
 #аргумента, и возвращает сумму наибольших двух аргументов.
+
+def fun(x, y, z):
+    suma = [x, y, z]
+    total = []
+    a = max(suma)
+    total.append(a)
+    suma.remove(a)
+    b = max(suma)
+    total.append(b)
+
+    print(sum(total))
+print(fun(int(input("Введите первое число:")), int(input("Введите второе число: ")), int(input("Введите третье число:"))))
