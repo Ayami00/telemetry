12e7fbe108a5286fc47c324df57acd743c17dc02
Ayami00 <I79606606960@yandex.ru>
2022-02-26 11:43:56 +0300

    2

---
+++ Zadanie___3/classw.py
@@ -0,0 +1,52 @@
+
+#def fun(number1, number2):
+    # тело функции
+  #  result = number1 + number2
+  #  return result#возвращенное значение
+#print(fun(2, 3))
+#print(fun(3, 3))
+#----------------------------------------------------------
+#def fun(number):
+#    resault = 1
+ #   for i in number:
+  #      resault += 1
+     #   return resault
+    #sp = [1, 3, 5, 6, 8, 8]
+  #  dig = input('Введите числа').split(' ')
+   # li = map(int, dig)
+   # resault = fun(li)
+#
+   # print(resault)
+#----------------------------------------------------
+#def fun(number1, number2):
+  #  resault = 1
+  #  for i in number1:
+   #   resault += 1
+#
+  #  for i in number2:
+     #resault += 1
+    #return resault
+#sp = [1, 3, 5, 6, 8, 8]
+#sp1 = [1, 3, 5, 6, 8, 8]
+#resault = fun(number1=sp, number2= sp1)
+#print(resault)
+#--------------------------------------------------------
+#def fun(number1, number2=1):
+   # resault = ' '.join(number1) * number2
+   # return resault
+#sp = [1, 3, 5, 6, 8, 8]
+#li = map(str, sp)
+#resault = fun(li, 20)
+#print(resault)
+#-----------------------------------------------------------
+#fun = lambda p1, p2 : p1+p2
+#print(fun(2, 3))
+#-----------------------------------------------------------------------
+#print(abs(-10))
+#print(min(-10, 10))
+#print(max(-10,10))
+#print(sum([1,3]))
+#print(pow(16,1/2))
+#----------------------------------------------------------------
+#for i in range(6, 20, 3):
+   # print(i)
